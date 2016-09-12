7b6b425e7bd9d12c1f2f2dfec1e999e6a8e37bc7
Camille Douay <camille.douay@gmail.com>
2016-09-12 15:30:46 +0200

    add package.json file

--- index.js
+++ index.js
@@ -78,12 +78,12 @@ function receivedMessage(event) {
   var messageAttachments = message.attachments;
 
   if (messageText) {
+	  console.log(messageText);
 
     // If we receive a text message, check to see if it matches any special
     // keywords and send back the corresponding example. Otherwise, just echo
     // the text we received.
     switch (messageText) {
-		console.log(messageText);
 		case 'bonjour': 
 		sendHelloWordMessage(senderID);
 		break;
